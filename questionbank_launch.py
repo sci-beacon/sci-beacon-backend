ea08c2bb90bbdb23dd4cc5fd6b254de65ee72fcf
answerquest <nikhil.js@gmail.com>
2025-11-29 18:56:13 +0530

    update

--- questionbank_launch.py
+++ questionbank_launch.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt='%Y-%m-%d %H:%M:%S',
     # level=logging.INFO,
-    filename=os.path.join(root, "log.txt"),
+    filename=os.path.join(root, 'logs', "log.txt"),
 )
 
 #########################################################
